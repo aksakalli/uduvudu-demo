350bf6962369e5a11a4af2f53b2b087695aa90c2
Can Guney Aksakalli <cgaksakalli@gmail.com>
2015-12-08 00:26:36 +0100

    resolving identifiers from hash url

--- js/main.js
+++ js/main.js
@@ -1,41 +1,53 @@
-loadPerson('ChristophLange');
-
-
-$("#username-search").typeahead({
+$('#username-search').typeahead({
   source: people,
   updater: function(selected) {
-    loadPerson(selected);
+    document.location = '#/' + selected;
     console.log('Loading person:' + selected);
   }
 });
 
-$("form").submit(function(event) {
+$('form').submit(function(event) {
   event.preventDefault();
 });
 
+$(window).on('hashchange', hashUrlHandler);
+$(document).ready(hashUrlHandler);
+
+function hashUrlHandler() {
+  if (location.hash) {
+    var identifier = location.hash.slice(2);
+    loadPerson(identifier);
+    $('#username-search').val(identifier);
+    document.title = identifier + ' | Uduvudu Demo';
+  } else {
+
+  }
+}
 
 function loadPerson(username) {
+  //show loading animation
+  $('#main').html('<i class="glyphicon glyphicon-refresh glyphicon-spin"></i>');
+
   var resource = 'http://eis.iai.uni-bonn.de/' + username;
   var source = 'people.n3';
 
   var store = new rdf.LdpStore();
   // prepare visualizer templates for uduvudu
-  $("#templates").load("templates.html");
+  $('#templates').load('templates.html');
 
   // load triples
   store.graph(source, function(graph, error) {
     if (error == null) {
-      console.debug("successfully loaded " + graph.toArray().length + " triples");
+      console.debug('successfully loaded ' + graph.toArray().length + 'triples');
       // resource (entry for template search) is same as source in this example
       uduvudu.process(graph, {
         'resource': resource
       }, function(out) {
         // write the result of the processing in the main div
-        //console.log(out);
         $('#main').html(out);
       });
     } else {
-      document.getElementById('main').innerHTML = '' + '<div class="alert alert-danger">' + '  <button type="button" class="close" data-dismiss="alert">&times;</button>' + '  <strong>Error:</strong> ' + error + '.' + '</div>';
+      $('#main').html('<div class="alert alert-warning" role="alert">Error: ' + error + '</div>');
     };
   });
 }
